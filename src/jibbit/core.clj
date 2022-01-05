2999b25ad0d7cdce7816c00e07123efdd8896ef2
Jim Clark <jim@atomist.com>
2022-01-05 13:45:14 -0800

    Handle git libs

--- src/jibbit/core.clj
+++ src/jibbit/core.clj
@@ -16,7 +16,7 @@
                                    Credential
                                    LayerConfiguration
                                    LogEvent)
-   (com.google.cloud.tools.jib.api.buildplan AbsoluteUnixPath)
+   (com.google.cloud.tools.jib.api.buildplan AbsoluteUnixPath ModificationTimeProvider FileEntriesLayer)
    (com.google.cloud.tools.jib.frontend
     CredentialRetrieverFactory)
    (java.nio.file Paths)
@@ -24,12 +24,16 @@
    (java.util List ArrayList Optional)
    (java.util.function Consumer)))
 
-(defn libs [{:keys [classpath]}]
+(defn libs 
+  "Files for each lib in the classpath (often jars for :mvn deps, and dirs for :git libs)"
+  [{:keys [classpath]}]
   (->> classpath
        (filter #(-> % val :lib-name))
        (map (comp io/file key))))
 
-(defn manifest-class-path [{:keys [classpath] :as basis}]
+(defn manifest-class-path 
+  "just the libs (not the source or resource paths) relative to the WORKDIR"
+  [{:keys [classpath] :as basis}]
   (->> (libs basis)
        (map #(str "lib/" (.getName %)))
        (clojure.string/join " ")))
@@ -40,7 +44,11 @@
        (map key)
        (into [])))
 
-(defn container-cp [basis] 
+(defn container-cp 
+  "container classpath (suitable for -cp)
+   paths are relative to container WORKDIR, 
+   libs are copied into WORKDIR/lib"
+  [basis] 
   (->> (paths basis)
        (concat (->> (libs basis)
                     (map #(str "lib/" (.getName %)))))
@@ -79,10 +87,27 @@
     :else
     target-image))
 
+;; assumes aot-ed jar is in root of WORKDIR
+(defn entry-point 
+  [{:keys [basis aot jar-name main]}]
+  (-> ["java" "-Dclojure.main.report=stderr" "-Dfile.encoding=UTF-8"]
+      (concat
+       (-> basis :classpath-args :jvm-opts))
+      (concat
+       (if aot
+         ["-jar" jar-name]
+         (concat
+          ["-cp" (container-cp basis) "clojure.main"]
+          (if-let [main-opts (-> basis :classpath-args :main-opts)]
+            main-opts
+            ["-m" (pr-str main)]))))))
+
 (defn jib-build
   "Containerize using jib
      - dependent jar layer:  copy all dependent jars from target/lib into WORKING_DIR/lib
-     - app jar layer:  copy target/app.jar into WORKING_DIR
+     - app layer:  
+         if aot copy target/app.jar into WORKING_DIR
+         else copy source/resource paths too
      - try to set a non-root user
      - add org.opencontainer LABEL image metadata from current HEAD commit"
   [{:keys [git-url base-image target-image jar-name jar-file tag aot main basis debug]
@@ -100,39 +125,50 @@
       (.addLabel "org.opencontainers.image.revision" (clojure.string/trim (:out (sh/sh "git" "rev-parse" "HEAD"))))
       (.addLabel "org.opencontainers.image.source" git-url)
       (.addLabel "com.atomist.containers.image.build" "clj -Tjib build")
-        ;; first layer has all the dependent jars
-      (.addLayer
-       (apply into-list (->> (libs basis)
-                             (filter #(.isFile %))
-                             (map #(get-path (.getPath %)))
-                             (into [])))
-       (AbsoluteUnixPath/get "/lib"))
-      ;; second layer has the compiled jar
-      (.addLayer
-       (apply into-list (if aot
-                          [(get-path jar-file)]
-                          (->> (paths basis)
-                               (map get-path)
-                               (into []))))
-       (AbsoluteUnixPath/get "/"))
-        ;; TODO should we add layer comments for downstream analyzers?
-      #_(.addLayer (-> (LayerConfiguration/builder)
-                       (.setName "Custom Java Layer")
-                       (.addEntry (get-path standalone-jar) (AbsoluteUnixPath/get "/"))
-                       (.build)))
-        ;; TODO this might add too much confusion - can we keep this simple and just not add this?
-      #_(.setProgramArguments (into-list "server-0.1.1-standalone.jar"))
+      ;; first layer has all the dependent jars
+      (.addFileEntriesLayer
+       (-> (FileEntriesLayer/builder)
+           (.setName "dependencies layer")
+           ((fn [layer-builder]
+              (->> (libs basis)
+                   (map #(if (.isDirectory %)
+                           (.addEntryRecursive
+                            layer-builder
+                            (Paths/get (.getPath %) (into-array String []))
+                            (AbsoluteUnixPath/get (format "/lib/%s" (.getName %))))
+                           (.addEntry
+                            layer-builder
+                            (Paths/get (.getPath %) (into-array String []))
+                            (AbsoluteUnixPath/get (format "/lib/%s" (.getName %))))))
+                   (doall))
+              layer-builder))
+           (.build)))
+      ;; second layer has the compiled jar or the source paths (depending on whether this is doing aot)
+      (.addFileEntriesLayer
+       (-> (FileEntriesLayer/builder)
+           (.setName "clojure application layer")
+           ((fn [layer-builder]
+              (if aot
+                (.addEntry
+                 layer-builder
+                 (get-path jar-file)
+                 (AbsoluteUnixPath/get (format "/%s" jar-name)))
+                (->> (paths basis)
+                     (map get-path)
+                     (map #(.addEntryRecursive
+                            layer-builder
+                            %
+                            (AbsoluteUnixPath/get (format "/%s" (-> % (.toFile) (.getName))))))
+                     (doall)))
+              layer-builder))
+           (.build)))
       (set-user (assoc c :base-image base-image))
-      (.setEntrypoint (apply into-list (-> ["java" "-Dclojure.main.report=stderr" "-Dfile.encoding=UTF-8"]
-                                           (concat
-                                            (if aot
-                                              ["-jar" jar-name]
-                                              ["-cp" (container-cp basis) "clojure.main" "-m" (pr-str main)])))))
+      (.setEntrypoint (apply into-list (entry-point c)))
       (.containerize (-> (Containerizer/to (-> (merge target-image (when tag {:tag tag}))
                                                (add-tags)
                                                (configure-image {:name "clj-jib-test"})))
                          (.setToolName "clojure jib builder")
-                         (.setToolVersion "0.1.8")
+                         (.setToolVersion "0.1.10")
                          (.addEventHandler
                           LogEvent
                           (reify Consumer
@@ -151,7 +187,10 @@
                         (io/file dir default-jibbit-config-file))]
     (edn/read-string (slurp edn-file))))
 
-(defn aot-clj [{:keys [jar-file basis] :as jib-config}]
+(defn aot-clj 
+  "aot compile and jar the paths and resources - not an uberjar
+    Class-Path manifest references all mvn libs"
+  [{:keys [jar-file basis] :as jib-config}]
   (println "... clojure.tools.build.api/compile-clj")
   (b/compile-clj {:src-dirs (paths basis)
                   :class-dir class-dir
@@ -178,10 +217,12 @@
                     c
                     {:jar-file (format "target/%s" jar-name)
                      :jar-name jar-name
-                     :basis basis }
+                     :basis basis}
                     (dissoc params :config))]
-    (when (not (:main jib-config))
-      (throw (ex-info "must specify :main config" {})))
+    (when (and
+            (not (-> basis :classpath-args :main-opts))
+            (not (:main jib-config)))
+      (throw (ex-info "config must specify either :main or an alias with :main-opts" {})))
     (when (:aot jib-config)
       (aot-clj jib-config))
     (println "... run jib")
