6542ddc2740ecd0a25bf7cad6a602fa2f66209e5
Jim Clark <jim@atomist.com>
2022-03-16 18:55:44 -0700

    Supporting skaffold
    * support file ownership for code layer - when pod runs non-root,
      skaffold needs this for file sync
    * add tagger that can use an environment variable for IMAGE name (from
      skaffold environment)

--- src/jibbit/tagger.clj
+++ src/jibbit/tagger.clj
@@ -2,11 +2,25 @@
   (:require [clojure.string :as s]
             [clojure.tools.build.api :as b]))
 
-(defn tag [{:keys [local]}]
-  (if local
-    (s/trim (slurp local))
-    (if (b/git-process {:dir b/*project-root* :git-args ["status" "--porcelain"]})
-      (throw (ex-info "jibbit.tagger/tag requires a clean working directory" {}))
-      (let [sha (b/git-process {:dir b/*project-root* :git-args ["rev-parse" "HEAD"]})
-            tag (b/git-process {:dir b/*project-root* :git-args ["describe" "--tags" "--exact-match"]})]
-        (or tag sha)))))
+(defn local-file
+  "slurp the image tag from a local version file"
+  [{:keys [image-name file]}]
+  (let [[_ n _] (re-find #"(.*):(.*)" image-name)]
+    (format "%s:%s" (or n image-name) (s/trim (slurp file)))))
+
+(defn tag
+  "update the tag of the image-name 
+   with the output of 'git describe --tags --exact-match' 
+   or the commit sha if there's no tag match"
+  [{:keys [image-name]}]
+  (if (b/git-process {:dir b/*project-root* :git-args ["status" "--porcelain"]})
+    (throw (ex-info "jibbit.tagger/tag requires a clean working directory" {}))
+    (let [sha (b/git-process {:dir b/*project-root* :git-args ["rev-parse" "HEAD"]})
+          tag (b/git-process {:dir b/*project-root* :git-args ["describe" "--tags" "--exact-match"]})
+          [_ n _] (re-find #"(.*):(.*)" image-name)]
+      (format "%s:%s" (or n image-name) (or tag sha)))))
+
+(defn environment 
+  "use an environment variable for the image name"
+  [{:keys [varname]}]
+  (.get (System/getenv) varname))
